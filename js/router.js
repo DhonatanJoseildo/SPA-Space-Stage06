d21e3c1d634fd0766e47f622c579e62d9704c4c5
Dhonatan Joseildo <dhon.joseildo@gmail.com>
2022-07-31 23:15:46 -0300

    CreatedRepository

---
+++ js/router.js
@@ -0,0 +1,47 @@
+export class Router {
+  routes = {}
+
+  add(routeName, page){
+    this.routes[routeName] = page
+  }
+
+  route(event) {
+    // certificando que esta pegando o event!
+    event = event || window.event
+    // negando a ação padrão do evento que seria redirecionar para a proxima rota
+    event.preventDefault()
+  
+    // colocando no historico da janela os nomes das rotas, ja que o preventDefault impede isso tambem!
+    window.history.pushState({}, "", event.target.href)
+  
+    // chamando uma function para manipular os dados
+    this.handle()
+  }
+  
+  handle() {
+    const bodyApp = document.querySelector('body')
+    // pegando o nome da rota ou url
+    const { pathname } = window.location
+    const route = this.routes[pathname] || this.routes[404]
+
+    if (route == "/pages/home.html") {
+
+      bodyApp.classList.remove('universo', 'exploracao')
+
+    }else if (route == "/pages/universo.html") {
+
+      bodyApp.classList.add('universo')
+      
+    }else if (route == "/pages/exploracao.html") {
+
+      bodyApp.classList.add('exploracao')
+    }
+
+    fetch(route)
+    .then(data => data.text())
+    .then(html => {
+      document.querySelector('#app').innerHTML = html
+    })
+  }
+
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# instalando node no projeto comandos
+- npm init -y (para ocmeçar o projeto)
+- npm install lite-server (para criar um mine server)
+
+## tema spaço e exploração
+- para aprimoramento no conhecimento com node.js
