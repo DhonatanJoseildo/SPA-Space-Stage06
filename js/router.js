0ee2bac1f4e433257c363871f6d5c1b87eb38345
Dhonatan Joseildo <dhon.joseildo@gmail.com>
2022-08-01 21:47:29 -0300

    finish

--- js/router.js
+++ js/router.js
@@ -20,6 +20,10 @@ export class Router {
   
   handle() {
     const bodyApp = document.querySelector('body')
+    const a1Event = document.querySelector('nav .navigation a.a1')
+    const a2Event = document.querySelector('nav .navigation a.a2')
+    const a3Event = document.querySelector('nav .navigation a.a3')
+    
     // pegando o nome da rota ou url
     const { pathname } = window.location
     const route = this.routes[pathname] || this.routes[404]
@@ -27,15 +31,26 @@ export class Router {
     if (route == "/pages/home.html") {
 
       bodyApp.classList.remove('universo', 'exploracao')
-
+      
+      a1Event.classList.add('checked')
+      a2Event.classList.remove('checked')
+      a3Event.classList.remove('checked')
     }else if (route == "/pages/universo.html") {
 
       bodyApp.classList.add('universo')
-      bodyApp.classList.remove('exlporacao')
+      bodyApp.classList.remove('exploracao')
+
+      a2Event.classList.add('checked')
+      a1Event.classList.remove('checked')
+      a3Event.classList.remove('checked')
     }else if (route == "/pages/exploracao.html") {
 
       bodyApp.classList.add('exploracao')
       bodyApp.classList.remove('universo')
+
+      a3Event.classList.add('checked')
+      a1Event.classList.remove('checked')
+      a2Event.classList.remove('checked')
     }
 
     fetch(route)
